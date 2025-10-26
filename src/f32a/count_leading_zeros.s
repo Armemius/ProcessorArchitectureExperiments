2ad7fe26850670e3591769f0a6a724daf2da91cb
Armemius <rusgy@yandex.ru>
2025-10-27 00:22:47 +0300

    feat: provide f32a count_leading_zeros solution

---
+++ src/f32a/count_leading_zeros.s
@@ -0,0 +1,31 @@
+    .data
+
+input_addr:      .word  0x80
+output_addr:     .word  0x84
+const_high_bit:  .word  0x80000000
+
+    .text
+
+_start:
+    @p input_addr a! @
+    dup
+    if zero                  \ Edge case: input = 0 -> result = 32
+    lit 0 a!                 \ A <- 0
+
+loop:
+    dup lit -1 xor           \ [a] -> [a, ~a]
+    @p const_high_bit and    \ [a, ~a] -> [a, b = ~a & const_high_bit]
+    if end                   \ [a, b] -> [a], if (b == 0) end
+    lit 1 a + a!             \ A <- A + 1
+    2*                       \ [a] -> [a = a << 2]
+    loop ;
+
+zero:
+    lit 32 a!                \ [] -> [32]
+end:
+    a @p output_addr a! !
+    drop
+    halt
+
+
+
